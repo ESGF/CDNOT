bad983664e3b853e14b8829ca5abf899b2edfb2a
Elysia Witham <e.witham@columbia.edu>
2021-06-25 15:23:22 -0400

    added add-repo script and also an update-docs backup

---
+++ add-repo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BASEDIR=/export/witham3/cdnot/docs
+REPODIR=/export/witham3/docs-repos
+REPOURL=$1
+REPOGIT=${REPOURL##*/}
+REPO="${REPOGIT%.*}"
+TEMPLATE="for FILE in $REPO/docs/*
+do
+  fn=\$(basename \$FILE)
+  bash update-repo.sh $REPO \$fn
+done"
+
+cd $REPODIR
+git clone $REPOURL
+
+if [ -z "$2" ]
+then
+  echo "$TEMPLATE" >> update-docs.sh
+else
+  FN=$2
+  echo "bash update-repo.sh $REPO $FN" >> update-docs.sh
+fi
+cd $BASEDIR
